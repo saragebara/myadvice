5e1c57c5196d574f797ed97de09df2e8f7e690ff
Sara Gebara <saragebaraa@gmail.com>
2026-03-08 16:51:22 -0400

    Initial project setup - DB entities, repositories, and folders for curriculum advising/bookings

---
+++ src/main/java/com/sad/myadvice/MyadviceApplication.java
@@ -0,0 +1,13 @@
+package com.sad.myadvice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyadviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyadviceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/sad/myadvice/repository/PrerequisiteRepository.java
@@ -0,0 +1,11 @@
+package com.sad.myadvice.repository;
+
+import com.sad.myadvice.entity.Prerequisite;
+import com.sad.myadvice.entity.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface PrerequisiteRepository extends JpaRepository<Prerequisite, Long> {
+    List<Prerequisite> findByCourse(Course course);
+    List<Prerequisite> findByCourseAndType(Course course, Prerequisite.Type type);
+}
