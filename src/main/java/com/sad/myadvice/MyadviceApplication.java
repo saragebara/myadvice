4994aea2769e2fc285ae67d1e473bf5597e168bf
Sara Gebara <saragebaraa@gmail.com>
2026-03-16 16:11:30 -0400

    JavaFX GUI Test

--- src/main/java/com/sad/myadvice/MyadviceApplication.java
+++ src/main/java/com/sad/myadvice/MyadviceApplication.java
@@ -1,13 +1,42 @@
 package com.sad.myadvice;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-public class MyadviceApplication {
+public class MyadviceApplication extends Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MyadviceApplication.class, args);
-	}
+    private ConfigurableApplicationContext springContext;
 
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void init() {
+        springContext = SpringApplication.run(MyadviceApplication.class);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/MainView.fxml"));
+
+        // Let Spring inject dependencies into the controller
+        loader.setControllerFactory(springContext::getBean);
+
+        Scene scene = new Scene(loader.load());
+        stage.setTitle("myAdvice");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @Override
+    public void stop() {
+        springContext.close();
+    }
 }
